6d43560f8593242fe6d4f13a651d9e3aa0bed6d5
Fabiano Teixeira <fabianotxss@gmail.com>
2024-10-05 10:35:21 -0300

    add connection string postgres

---
+++ src/FIAP.PhaseOne.Infra/Context/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FIAP.PhaseOne.Infra.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+              
+        }
+    }
+}
---
+++ src/FIAP.PhaseOne.Infra/InfrastructureServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using FIAP.PhaseOne.Infra.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FIAP.PhaseOne.Infra
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfraServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("Default")));
+            return services;
+        }
+    }
+}
