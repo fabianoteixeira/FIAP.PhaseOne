f437141f00622bfde9c518952c29ec33ba6a17a1
Gustavo de Oliveira <100207017+gugol94@users.noreply.github.com>
2024-11-02 11:06:15 -0300

    removido comentários

--- src/FIAP.PhaseOne.Api/Program.cs
+++ src/FIAP.PhaseOne.Api/Program.cs
@@ -4,10 +4,7 @@ using FIAP.PhaseOne.Application.Shared;
 using FIAP.PhaseOne.Api.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfraServices(builder.Configuration);
@@ -16,7 +13,7 @@ builder.Services.AddApiService();
 
 
 var app = builder.Build();
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
